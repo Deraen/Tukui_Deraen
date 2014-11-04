410f896e5ed7ffb1cf16c94b0ba1d92237067601
Juho Teperi <juho.teperi@iki.fi>
2014-11-05 00:16:47 +0200

    T16

--- config.lua
+++ config.lua
@@ -1,4 +1,20 @@
-local addon, ns = ...
+local T, C, L = Tukui:unpack()
 
-ns.config={
-}
+C["ActionBars"]["HotKey"] = true
+
+C["Auras"]["Consolidate"] true
+
+C["Bags"]["Enable"] = false
+
+C["Chat"]["Background"] = true
+
+C["DataTexts"]["Time24HrFormat"] = true
+
+C["Merchant"]["UseGuildRepair"] = true
+
+C["Raid"]["ShowPets"] = false
+
+C["Tooltips"]["MouseOver"] = true
+
+C["UnitFrames"]["Portrait"] = true
+C["UnitFrames"]["UnlinkCastBar"] = true
